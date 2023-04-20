a38b9bb55ccad9057390812b9e043ca5293f82a6
smartyfeed <kolya.fomichev@me.com>
2023-04-21 00:20:33 +0200

    Remove SCSS preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,10 @@
 import adapter from "@sveltejs/adapter-static";
-// import preprocess from "svelte-preprocess";
 /**
  * This will add autocompletion if you're working with SvelteKit
  *
  * @type {import('@sveltejs/kit').Config}
  */
 const config = {
-  // preprocess: preprocess({
-  //   scss: {
-  //     includePaths: ['node_modules'],
-  //     prependData: `@import 'static/global.scss';`,
-      
-  //   },
-  // }),
   kit: {
     adapter: adapter(),
   },
