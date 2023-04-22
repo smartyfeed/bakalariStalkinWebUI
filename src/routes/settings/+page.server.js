5c4a912e33f010ccbb54c0fc9fd37d9b2ac727e6
smartyfeed <kolya.fomichev@me.com>
2023-04-22 21:38:45 +0200

    Settings page implemented

--- src/routes/settings/+page.server.js
+++ src/routes/settings/+page.server.js
@@ -1,19 +1,113 @@
-import { post } from '$lib/global.js';
+// @ts-nocheck
+import { post, get } from '$lib/global.js';
 import { fail } from '@sveltejs/kit';
+var res = {};
+var inflow = false;
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ cookies }) {
+	if (!inflow) {
+		let response = await get('/settings', { token: cookies.get('token') });
+		res.settings = response.settings;
+		res.step = 4;
+		if (!res.settings.bakaServer || !res.settings.className || !res.settings.groups) {
+			res.step = 1;
+			return res;
+		}
+		res.settings.groups = JSON.parse(res.settings.groups);
+		res.classes = response.classes;
+		res.groups = response.groups;
+	}
+	return res;
+}
 
 /** @type {import('./$types').Actions} */
 export const actions = {
 	default: async ({ cookies, request }) => {
 		const data = await request.formData();
-		let token = cookies.get('token');
-		data.append('token', token);
-		var response = await post('/updateSettings', data, {}, false);
-		var responseJSON = await response.json();
+		if (
+			(data.get('bakaServer') != res.settings.bakaServer && res.step == 4) ||
+			(data.get('className') != res.settings.className && res.step == 4) ||
+			data.get('step') == 1
+		) {
+			inflow = true;
 
-		if (response.status != 200) {
-			return fail(response.status, { error: responseJSON.error, message: responseJSON.message });
+			let response = await post('/server', data, { token: cookies.get('token') }, false);
+			let responseJSON = await response.json();
+			if (response.status != 200) {
+				res.step = 1;
+				return fail(response.status, {
+					error: responseJSON.error,
+					message: responseJSON.message
+				});
+			}
+
+			res.settings.bakaServer = responseJSON.bakaServer;
+			res.classes = responseJSON.classes;
+
+			res.step = 2;
+			return {
+				data: {
+					inflow: true
+				}
+			};
 		}
 
-		return { success: true };
+		if (data.get('step') == 2) {
+			data.set('bakaServer', res.settings.bakaServer);
+			let response = await post('/groups', data, { token: cookies.get('token') }, false);
+			let responseJSON = await response.json();
+
+			if (response.status != 200) {
+				res.step = 2;
+				return fail(response.status, {
+					error: responseJSON.error,
+					message: responseJSON.message
+				});
+			}
+			res.groups = responseJSON;
+
+			res.settings.className = data.get('className');
+
+			res.step = 3;
+			return {
+				data: {
+					inflow: true
+				}
+			};
+		}
+		if (data.get('step') >= 3) {
+			data.set('bakaServer', res.settings.bakaServer);
+			let response = await post('/updateSettings', data, { token: cookies.get('token') }, false);
+			let responseJSON = await response.json();
+
+			if (response.status != 200) {
+				switch (responseJSON.error) {
+					case 'E_BAD_BAKA_SERVER':
+						res.step = 1;
+						break;
+					case 'E_BAD_CLASS_NAME':
+						res.step = 2;
+						break;
+					case 'E_BAD_GROUPS':
+						res.step = 3;
+					default:
+						res.step = 1;
+				}
+				return fail(response.status, {
+					error: responseJSON.error,
+					message: responseJSON.message
+				});
+			}
+
+			res.step = 4;
+			inflow = false;
+			return {
+				data: {
+					inflow: false
+				},
+				success: true
+			};
+		}
 	}
 };
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,69 +1,75 @@
 <script>
 	import { enhance } from '$app/forms';
-	import { get } from '$lib/global.js';
-	var settingsPromise = get('/settings');
+	/** @type {import('./$types').PageData} */
+	export let data;
 
 	/** @type {import('./$types').ActionData} */
 	export let form;
 </script>
 
-{#await settingsPromise}
-	<p>loading</p>
-{:then settings}
-	{#if form?.success}
-		<div class="alert alert-success" role="alert">Successfully saved!</div>
-	{/if}
-	{#if form?.error == 'E_BAD_BAKA_SERVER'}<div class="alert alert-danger" role="alert">
-			Invalid/unsupported server address!
-		</div>{/if}
-	{#if form?.error == 'E_BAD_CLASS_NAME'}<div class="alert alert-danger" role="alert">
-			Incorrect class name!
-		</div>{/if}
-	{#if form?.error == 'E_BAD_GROUPS'}<div class="alert alert-danger" role="alert">
-			Invalid groups!
-		</div>{/if}
-	<h3>Personal defaults</h3>
-	<form
-		method="POST"
-		use:enhance={({ form, data, action, cancel, submitter }) => {
-			return async ({ result, update }) => {
-        update({reset: false});
-			};
-		}}
-	>
-		<div class="mb-3">
-			<label for="bakaServer" class="form-label">Bakaláři server address</label>
-			<input
-				type="text"
-				class="form-control"
-				id="bakaServer"
-				name="bakaServer"
-				placeholder="https://is.sssvt.cz/IS/"
-				value={settings.bakaServer ?? ''}
-			/>
-		</div>
+{#if form?.success}
+	<div class="alert alert-success" role="alert">Successfully saved!</div>
+{/if}
+{#if form?.error == 'E_BAD_BAKA_SERVER'}<div class="alert alert-danger" role="alert">
+		Invalid/unsupported server address!
+	</div>{/if}
+{#if form?.error == 'E_BAD_CLASS_NAME'}<div class="alert alert-danger" role="alert">
+		Incorrect class name!
+	</div>{/if}
+{#if form?.error == 'E_BAD_GROUPS'}<div class="alert alert-danger" role="alert">
+		Invalid groups!
+	</div>{/if}
+<h3>Personal defaults</h3>
+<form
+	method="POST"
+	use:enhance={({ form, data, action, cancel, submitter }) => {
+		return async ({ result, update }) => {
+			update({ reset: false });
+		};
+	}}
+>
+	<input type="hidden" id="step" name="step" value={data.step} />
+	<div class="mb-3">
+		<label for="bakaServer" class="form-label">Bakaláři server address</label>
+		<input
+			type="text"
+			class="form-control"
+			id="bakaServer"
+			name="bakaServer"
+			value={data.settings.bakaServer ?? ''}
+		/>
+	</div>
+	{#if data.step > 1 && !form?.error}
 		<div class="mb-3">
 			<label for="className" class="form-label">Your class</label>
-			<input
-				type="text"
-				class="form-control"
-				id="className"
-				name="className"
-				placeholder="P4.B"
-				value={settings.className ?? ''}
-			/>
+			<select class="form-select" id="className" name="className">
+				{#each data.classes as className}
+					<option value={className.name} selected={className.name == data.settings.className}>
+						{className.name}
+					</option>
+				{/each}
+			</select>
 		</div>
+	{/if}
+	{#if data.step > 2 && !form?.error}
 		<div class="mb-3">
 			<label for="groups" class="form-label">Your groups</label>
-			<input
-				type="text"
-				class="form-control"
-				id="groups"
-				name="groups"
-				placeholder="1.sk, 2.sk... or leave empty for the whole class"
-				value={settings.groups ? JSON.parse(settings.groups).join(', ') : ''}
-			/>
+			<select class="form-select" id="groups" name="groups" multiple>
+				{#if form?.data.inflow}
+					<option value="">None</option>
+					{#each data.groups as group}
+						<option value={group}>{group}</option>
+					{/each}
+				{:else}
+					<option value="" selected={data.settings.groups.length == 0}>None</option>
+					{#each data.groups as group}
+						<option value={group} selected={data.settings.groups.includes(group)}>{group}</option>
+					{/each}
+				{/if}
+			</select>
 		</div>
-		<button type="submit" class="btn btn-primary">Submit</button>
-	</form>
-{/await}
+	{/if}
+	<button type="submit" class="btn btn-primary"
+		>{data.step >= 3 && !form?.error ? 'Submit' : 'Next'}</button
+	>
+</form>
