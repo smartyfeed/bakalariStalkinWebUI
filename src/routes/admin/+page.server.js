2afe4a4754904c50eced7b9966044abb6fc72214
smartyfeed <kolya.fomichev@me.com>
2023-04-28 14:17:52 +0200

    Redirect on unauthorized

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -1,6 +1,13 @@
 import { get } from '$lib/global.js';
+import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ cookies }) {
-	return await get('/admin', { token: cookies.get('token') });
+  let response = await get('/admin', { token: cookies.get('token') }, true);
+
+  if (response.status != 200) {
+    throw redirect(304, '/');
+  }
+
+  return {stalkers: response.stalkers}
 }
