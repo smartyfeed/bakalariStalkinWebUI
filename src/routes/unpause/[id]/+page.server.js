46301ba7975889a7fb0081230170493b21b0863d
smartyfeed <kolya.fomichev@me.com>
2023-06-26 19:15:55 +0200

    Remove debug logging

--- src/routes/unpause/[id]/+page.server.js
+++ src/routes/unpause/[id]/+page.server.js
@@ -1,11 +1,8 @@
 import { post } from '$lib/global';
-import { redirect } from '@sveltejs/kit';
 
 export async function load({ cookies, params }) {
-	console.log(params);
 	if (params.id) {
 		let body = { id: params.id, unpause: true };
-		console.log(body);
 		await post(`/pauseSub`, body, { token: cookies.get('token') }, false);
 	}
 }
