ebb4bbbcb33a555dbee5b773c3da73fe7d6686a9
smartyfeed <kolya.fomichev@me.com>
2023-06-26 18:48:54 +0200

    oh

--- src/routes/unpause/[id]/+page.svelte
+++ src/routes/unpause/[id]/+page.svelte
@@ -9,19 +9,3 @@
     window.location.replace("https://bakalari.smartyfeed.me");
 	});
 </script>
-
-<div class="container half-width">
-	{#if data.sub?.id}
-		<h3>Pozastavit odběr - {data.sub?.label}</h3>
-	{:else}
-		<h3>Pozastavit všechny odběry</h3>
-	{/if}
-	<form method="POST">
-		<input type="hidden" id="oldId" name="id" value={data.sub?.id || 'all'} />
-		<div class="mb-3">
-			<label for="until" class="form-label">Do</label>
-			<input id="until" name="until" type="text" class="form-control" required />
-		</div>
-		<button type="submit" class="btn btn-warning">Pozastavit</button>
-	</form>
-</div>
